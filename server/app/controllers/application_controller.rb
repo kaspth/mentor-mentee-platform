724f165eb7f1e94bc5e15b1183dae4d6be9bf5ca
Kasper Timm Hansen <kaspth@gmail.com>
2020-03-14 00:53:26 +0100

    See if Gems are cached.

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
-class ApplicationController < ActionController::API 
+class ApplicationController < ActionController::API
   #protect_from_forgery with: :exception
   include Response
   include ExceptionHandler
 
   def set_default_request_format
+    # Add a comment to tickle CI.
     request.format = :json
   end
   before_action :set_default_request_format
 end
-
